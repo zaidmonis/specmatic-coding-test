a490ec8935d97745c76b96f102b26ad8cb6efd62
Mohd Zaid <zaid@Mohds-MacBook-Pro-2.local>
2024-06-08 09:46:54 +0530

    Added Cost to product details and fixed additional tests

--- src/main/kotlin/com/store/repositories/ProductRepository.kt
+++ src/main/kotlin/com/store/repositories/ProductRepository.kt
@@ -3,17 +3,15 @@ package com.store.repositories
 import com.store.objects.Product
 import com.store.objects.ProductType
 
-class ProductRepository constructor(
-
-) {
+class ProductRepository {
     var products: Array<Product>
 
     init {
-        val book = Product(id = 0, name = "Harry Potter", type = ProductType.book, inventory=2)
-        val gadget = Product(id = 1, name = "Apple Iphone", type = ProductType.gadget, inventory=4)
-        val food = Product(id = 2, name = "Fruit Cake", type = ProductType.food, inventory=5)
-        val other = Product(id = 3, name = "Table", type = ProductType.other, inventory=1)
-        val other2 = Product(id = 4, name = "Pen", type = ProductType.other, inventory=9)
+        val book = Product(id = 0, name = "Harry Potter", type = ProductType.book, inventory=2, cost = 500)
+        val gadget = Product(id = 1, name = "Apple Iphone", type = ProductType.gadget, inventory=4, cost = 50000)
+        val food = Product(id = 2, name = "Fruit Cake", type = ProductType.food, inventory=5, cost = 40)
+        val other = Product(id = 3, name = "Table", type = ProductType.other, inventory=1, cost = 8000)
+        val other2 = Product(id = 4, name = "Pen", type = ProductType.other, inventory=9, cost = 10)
         products = arrayOf(book, gadget, food, other, other2)
     }
     fun getProducts(type: ProductType) : Array<Product> {
